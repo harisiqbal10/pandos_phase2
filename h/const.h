93ddcb06a3b72a05fa357ba5fa37c2c2b36553cc
Haris-Iqbal-coder <harisiqbal104@gmail.com>
2025-02-19 23:19:20 -0500

    updated const.h

--- h/const.h
+++ h/const.h
@@ -31,6 +31,12 @@
 #define MAXINT 0x7FFFFFFF   /* Maximum positive integer for 32-bit systems */
 #define CLOCKINTERVAL 100000UL
 
+/* Status Register Bit Masks */
+#define IEp 0x4        /* Enable Previous Interrupts (bit 2) */
+#define TE 0x08000000  /* Enable Local Timer (bit 27) */
+#define KUo 0xFFFFFFF7 /* Kernel Mode ON (clear bit 3) */
+#define RAMTOP 0x20001000 /* Top of RAM for stack initialization */
+
 /* device interrupts */
 #define DISKINT			  3
 #define FLASHINT 		  4
