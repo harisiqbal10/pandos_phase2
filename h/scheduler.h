fd926b7f37b350ea39f472add2a9942db14bfa46
Haris-Iqbal-coder <harisiqbal104@gmail.com>
2025-02-20 16:53:30 -0500

    updated scheduler.h

---
+++ h/scheduler.h
@@ -0,0 +1,22 @@
+#ifndef SCHEDULER
+#define SCHEDULER
+
+/************************* SCHEDULER.H *****************************
+ *
+ *  The externals declaration file for the Scheduler Module.
+ *
+ *  Implements a preemptive round-robin scheduling algorithm.
+ *  Handles process dispatching and deadlock detection.
+ */
+
+
+#include "../h/types.h"
+#include "../h/const.h"
+#include "../h/pcb.h"
+#include "../h/asl.h"
+
+extern void scheduler(); 
+
+/******************************************************************/
+
+#endif 
